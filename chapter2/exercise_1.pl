f5e6a804afb83ef4d4f63f27cb774a98e129d1ef
Steven Samson <kosai03@gmail.com>
2012-03-20 21:37:55 -0500

    First two exercises for chapter 2 added

---
+++ chapter2/exercise_1.pl
@@ -0,0 +1,8 @@
+#!/usr/local/bin/perl -w
+
+$pi = 3.141592654;
+$radius = 12.5;
+
+$circumference = 2 * $pi * $radius;
+
+print "The circumference of a circle with radius $radius is $circumference\n";
---
+++ chapter2/exercise_2.pl
@@ -0,0 +1,10 @@
+#!/usr/local/bin/perl -w
+
+$pi = 3.141592654;
+
+print "What is the radius of the circle? ";
+chomp($radius = <STDIN>);
+
+$circumference = 2 * $pi * $radius;
+
+print "The circumference of a circle with radius $radius is $circumference\n";
